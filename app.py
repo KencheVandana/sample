e8426f495bda4d7a02a55a2ee25392a690dabccf
KencheVandana <vandana.kenche@Triconinfotech.com>
2024-11-14 23:55:23 +0530

    Adding the server

--- app.py
+++ app.py
@@ -12,6 +12,6 @@ async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request, "title": "Welcome to FastAPI", "message": "Hello, FastAPI!"})
 
 # Run the application
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run(app, host="127.0.0.1", port=8000)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
